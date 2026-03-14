114700996bcecb0c44eaa3c4870481bfac604b57
Suja K <message@sujathan.ch>
2026-03-14 01:23:36 +0100

    yeah

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://ttssluhvsbauieeejsht.supabase.co',
   supabaseAnonKey: 'sb_publishable_6mZhCCjFD1izrq0Z_BAssw_e4v7zJpb',
-  pushVapidPublicKey: 'BDod_vvL8mRmFUgrej6kh1YRzqkluIIvd4pOidJJEtHv5-BCG5O3r7T8lXat3tF5fH6LUcfrqbF2EL--QK_emjk'
+  pushVapidPublicKey: 'BGwGmcY0NdbTA0-sAqImK3rffFCNtJ9zEGO90suuHuJCGw_cIm4nECqXxXoHYKXGf1RMFRx04dYk7ItEWwVaT9c'
 };
